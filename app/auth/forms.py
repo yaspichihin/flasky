d95bb2d48d1759243e7ce185783ef593eb1f9275
Yaroslav Spichihin <yaaspichikhin@rt-dc.ru>
2023-06-09 15:58:06 +0300

    add auth

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,44 +1,141 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
-from wtforms.validators import InputRequired, Email, Length, Regexp, EqualTo
+from wtforms.validators import InputRequired, Email, Length, Regexp, EqualTo, DataRequired
 
 from .. import db
 from ..models import User
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[InputRequired(), Length(1, 64), Email()])
-    password = PasswordField("Password", validators=[InputRequired()])
-    remember_me = BooleanField("Keep me logged in")
-    submit = SubmitField("Login")
-
-
-class RegistrationForm(FlaskForm):
-    email = StringField("email", validators=[InputRequired(), Length(1, 64), Email()])
-    username = StringField(
-        "Username",
+    email = StringField(
+        "Email",
         validators=[
             InputRequired(),
             Length(1, 64),
-            Regexp("^[A-Za-z]*$", 0, "Username must have only letters")
+            Email(),
         ]
     )
     password = PasswordField(
         "Password",
         validators=[
             InputRequired(),
-            EqualTo("password2", message="Password must match.")
         ]
     )
-    password2 = PasswordField("Confirm password", validators=[InputRequired()])
+    remember_me = BooleanField("Keep me logged in")
+    submit = SubmitField("Login")
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField(
+        "email",
+        validators=[
+            InputRequired(),
+            Length(1, 64),
+            Email(),
+        ]
+    )
+    username = StringField(
+        "Username",
+        validators=[
+            InputRequired(),
+            Length(1, 64),
+            Regexp("^[A-Za-z]*$", 0, "Username must have only letters"),
+        ]
+    )
+    password = PasswordField(
+        "Password",
+        validators=[
+            InputRequired(),
+            EqualTo(
+                "password2",
+                message="Password must match."
+            ),
+        ]
+    )
+    password2 = PasswordField(
+        "Confirm password",
+        validators=[
+            InputRequired(),
+        ]
+    )
     submit = SubmitField("Register")
 
-    @staticmethod
-    def validate_email(field):
+    def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
             raise ValidationError("Email already registered")
 
-    @staticmethod
-    def validate_username(field):
+    def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError("Username already in use.")
+
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField(
+        'Old password',
+        validators=[
+            DataRequired(),
+        ]
+    )
+    password = PasswordField(
+        'New password',
+        validators=[
+            DataRequired(),
+            EqualTo(
+                'password2',
+                message='Passwords must match.'
+            )
+        ]
+    )
+    password2 = PasswordField(
+        'Confirm new password',
+        validators=[
+            DataRequired(),
+        ]
+    )
+    submit = SubmitField('Update Password')
+
+
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Email(),
+        ]
+    )
+    submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(FlaskForm):
+    password = PasswordField(
+        'New Password',
+        validators=[
+            DataRequired(),
+            EqualTo(
+                'password2',
+                message='Passwords must match'
+            ),
+        ]
+    )
+    password2 = PasswordField(
+        'Confirm password',
+        validators=[DataRequired()]
+    )
+    submit = SubmitField('Reset Password')
+
+
+class ChangeEmailForm(FlaskForm):
+    email = StringField(
+        'New Email',
+        validators=[DataRequired(), Length(1, 64), Email()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()]
+    )
+    submit = SubmitField('Update Email Address')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data.lower()).first():
+            raise ValidationError('Email already registered.')
--- app/models.py
+++ app/models.py
@@ -1,7 +1,10 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from flask import current_app
 from . import login_manager
+
 
 # Указание моделей для базы данных
 class Role(db.Model):
@@ -23,6 +26,10 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
+
+    # relationship
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
 
     # password
     @property
@@ -36,8 +43,62 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    # relationship
-    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+    # confirmed
+    def generate_confirmation_token(self):
+        s = Serializer(current_app.config["SECRET_KEY"])
+        return s.dumps({"confirm": self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config["SECRET_KEY"])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get("confirm") != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
+    def generate_reset_token(self):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'reset': self.id})
+
+    @staticmethod
+    def reset_password(token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        user = User.query.get(data.get('reset'))
+        if user is None:
+            return False
+        user.password = new_password
+        db.session.add(user)
+        return True
+
+    def generate_email_change_token(self, new_email):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        return s.dumps(
+            {'change_email': self.id, 'new_email': new_email})
+
+    def change_email(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('change_email') != self.id:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return f"<User {self.username}>"
